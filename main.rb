8a638041b05341996aee1ef5d800dde7515767bb
Rafael F. Ribeiro <mikudesudesu@vivaldi.net>
2025-07-14 20:21:27 -0300

    - Adds get(key) to hash_map class - Adds append(node) to node clas  - I think it's implemented wrongly and its going to confuse me later, but for some reason it works in its weird way. Still needs to factor in load factor.

--- main.rb
+++ main.rb
@@ -6,6 +6,8 @@ require 'pry-byebug'
 class HashMap
   attr_accessor :load_factor, :capacity
 
+  PRIME_FACTOR = 31
+
   def initialize
     @load_factor = 0.75
     @capacity = 31
@@ -14,16 +16,35 @@ class HashMap
 
   def hash(key)
     hash_code = 0
-    prime_number = 31
-    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+    key.each_char { |char| hash_code = PRIME_FACTOR * hash_code + char.ord }
     hash_code.modulo(@capacity)
   end
 
   def set(key, value)
-    index = hash(key) & @buckets.size
-    bucket = @bucket[index]
-    p key
-    node = Node.new(value)
+    node = Node.new(key, value)
+    index = hash(key)
+    if @bucket[index].nil?
+      @bucket[index] = node
+    else
+      @bucket[index].append(node)
+    end
+  end
+
+  def get(key)
+    index = hash(key)
+    validation = @bucket[index] unless @bucket[index].nil?
+    until validation.next_node.nil?
+      if validation.key == key
+        return validation
+      else
+        return validation = validation.next_node
+      end
+      nil
+    end
+  end
+
+  def pry_byebug
+    binding.pry
   end
 end
 
@@ -31,13 +52,32 @@ end
 
 # Node class for hash_map
 class Node
-  attr_accessor :data, :next_node
+  attr_accessor :data, :key, :next_node
 
-  def initialize(data)
+  def initialize(key, data)
     self.data = data
+    self.key = key
     @next_node = nil
+  end
+
+  def append(node)
+    self.next_node = node
   end
 end
 
 abc = HashMap.new
-p abc.set('batata', 'carne')
+abc.set('apple', 'red')
+abc.set('banana', 'yellow')
+abc.set('carrot', 'orange')
+abc.set('dog', 'brown')
+abc.set('elephant', 'gray')
+abc.set('frog', 'green')
+abc.set('grape', 'purple')
+abc.set('hat', 'black')
+abc.set('ice cream', 'white')
+abc.set('jacket', 'blue')
+abc.set('kite', 'pink')
+abc.set('lion', 'golden')
+p abc.get('kite')
+p abc.get('apple')
+p abc.get('fantasma')
